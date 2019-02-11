d2ef5c89741fc1907982e21e01d1d624ecfb2bd2
Li Yaguang <46660697+yli2935@users.noreply.github.com>
2019-02-11 13:39:00 -0500

    name

---
+++ assignment2/Roll a ball/Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float forceScal=5;
+    private Rigidbody rb;
+    private int count;
+    private int number;
+    public Text countText;
+    public int point
+    {
+        get { return number; }
+        set { number = value; }
+    }
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        
+    }
+    void Update()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0f, moveVertical);
+        rb.AddForce(movement* forceScal);
+        
+    }
+   
+    private void OnTriggerEnter(Collider other)  
+    {
+        if (other.gameObject.tag == "pickUp"&&other.GetComponent<MeshRenderer>().material.GetColor("_Color")==Color.green) { point=1; }
+        else if(other.gameObject.tag == "pickUp" && other.GetComponent<MeshRenderer>().material.GetColor("_Color") == Color.red) { point=4; } 
+        else { point=2; }
+        Destroy(other.gameObject);
+        SetCountText();
+    }
+    void SetCountText()
+    {
+        count += point;
+        countText.text = "Count: " + count.ToString();
+    }
+
+    
+    
+}
